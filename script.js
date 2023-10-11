35f4f1f01a1afa1f5b40425df35605f787ae07b5
GalvasDev <146465630+VictorGalvaoMercante@users.noreply.github.com>
2023-10-11 09:42:44 -0300

    Projeto Pronto

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-function validarSenhas() {
-    var senha = document.getElementById("senha").value;
-    var confirmacao = document.getElementById("confirmacao").value;
-   
+function validarSenha() {
+    let senha = document.getElementById('senha').value;
+    let confirmacao = document.getElementById('confirmacao').value;
+    let msg = document.getElementById('msg')
 
 
-    let caracterEspecial = senha.includes("!" || "#" || "@" || "$" || "%" ||"&")
+    
 
-    // Verifica se as senhas são iguais
-    if (senha !== confirmacao) {
-        document.getElementById("containerResultado").innerHTML = "As senhas tem que ser iguais!"
-        return;
+    
+    
+    
+    if(senha == 'victor_34026355' && confirmacao == 'eric_35138921' || senha == "eric_35138921" && confirmacao == "victor_34026355"){
+        msg.innerHTML = "Nome dos criadores.OK!"
+    }
+    else if (senha !== confirmacao) {
+       msg.innerHTML = "As senhas tem que ser iguais!"
     }
 
-    // Verifica se a senha atende aos requisitos (por exemplo, ter pelo menos 8 caracteres)
-    if (senha.length < 6) {
-        document.getElementById("containerResultado").innerHTML= "A senha deve conter pelo menos 6 caracteres.";
-        return;
+    else if (senha.length < 6) {
+        msg.innerHTML = "A senha tem que ter no mínimo 6 dígitos!"
     }
 
-   
+    else if (!/[_@#$%&*]/.test(senha)) {
+        msg.innerHTML = "A senha tem que conter no mínimo um caracter especial (_, @, #, $, %, &, *)"
+    }
 
-    // Se as senhas coincidirem e atenderem aos requisitos, exiba uma mensagem de sucesso
-    document.getElementById("containerResultado").innerHTML= "As senhas são válidas.";
+    else if (!/[A-Z]/.test(senha)) {
+        msg.innerHTML = "A senha tem que conter no mínimo uma letra maiúscula!"
+    }
+
+    else if (!/[a-z]/.test(senha)) {
+        msg.innerHTML = "A senha tem que conter no mínimo uma letra minúscula!"
+    }
+
+    else if (!/\d/.test(senha)) {
+        msg.innerHTML = "A senha tem que conter no mínimo um número!"
+    }
+
+    else if (/(\w)\1\1/.test(senha)) {
+        msg.innerHTML = "A senha não pode conter 3 caracteres repetidos!"
+    }
+
+    else if (/(123|234|345|456|567|678|789)/.test(senha)) {
+        msg.innerHTML = "A senha não pode conter sequência de números (ex: 123, 789)"
+    }
+
+    else if (/(321|432|543|654|765|876|987)/.test(senha)) {
+        msg.innerHTML = "A senha não pode conter sequência decrescente de números (ex: 321, 987)"
+    }
+
+    else{
+        msg.innerHTML = "Senha cadastrada com sucesso!"
+    }
+    
+
 
 }
